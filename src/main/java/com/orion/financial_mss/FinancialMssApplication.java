ef93d5a0223881954e75ebb898c24298b562f7bf
Juergüen Herrera <jurg_92@hotmail.com>
2022-08-03 16:23:55 -0600

    after gitignore restructure

---
+++ src/main/java/com/orion/financial_mss/FinancialMssApplication.java
@@ -0,0 +1,13 @@
+package com.orion.financial_mss;
+
+import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMssApplication.class, args);
+	}
+
+}
